6a150e8ec977ee15a2658f63592b79fa26e6ce08
fcastano <fcastano@vicomtech.org>
2026-05-03 11:46:00 +0200

    Fixing volume bug for create mar

--- model_storage/create_mar.sh
+++ model_storage/create_mar.sh
@@ -7,10 +7,10 @@ mkdir -p model_storage
 torch-model-archiver \
   --model-name  blood_cell_detection \
   --version     1.0 \
-  --handler     /app/model/handler.py \
-  --extra-files "/app/model/adapter_manager.py,\
-/app/model/YOLO_adapter.py,\
-/app/model/custom_trainer.py,\
+  --handler     /app/src/model/handler.py \
+  --extra-files "/app/src/model/adapter_manager.py,\
+/app/src/model/YOLO_adapter.py,\
+/app/src/model/custom_trainer.py,\
 /app/model_storage/yolo_cfg/yolov8s.pt,\
 /app/model_storage/adapter_weights.pt" \
   --export-path /app/model_storage \
